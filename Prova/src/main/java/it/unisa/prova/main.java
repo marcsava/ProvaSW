66d7052210c01036505b4817088b0374819c9080
Marco <Marco@DESKTOP-SCE5RIG.mshome.net>
2021-11-10 11:54:16 +0100

    Prova branch

--- Prova/src/main/java/it/unisa/prova/main.java
+++ Prova/src/main/java/it/unisa/prova/main.java
@@ -10,8 +10,12 @@ package it.unisa.prova;
  */
 public class main {
     public static void main(String args[]) {
-        System.out.println("Hello world!");
-        System.out.println("Hello universe!");
+        salute("Fisciano");
+        salute("Universe");
+        salute("World");
+        
     }
-    
+    private static void salute(String name){
+        System.out.println("Hello, "+name);
+    }    
 }
